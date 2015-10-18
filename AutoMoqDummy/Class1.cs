61e316e818511aeebce948c0eff96783b6956d40
Hackle Wayne <hacklew@hotmail.com>
2015-10-18 19:44:38 +1300

    Commit

---
+++ AutoMoqDummy/Class1.cs
@@ -0,0 +1,43 @@
+﻿using Moq;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+using Ploeh.AutoFixture.NUnit2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMoqDummy
+{
+    public class Class1
+    {
+        [Test]
+        public void ThisShouldPass()
+        {
+            Assert.That(true, Is.True);
+        }
+
+        [Theory, AutoData]
+        public void TestAnyInt(int randomNumber)
+        {
+            Assert.That(randomNumber, Is.Not.Null);
+        }
+
+        [Theory, AutoMoqData]
+        public void TestAutoMoqFixture([Frozen]ISinger singer, string lyrics, Song song)
+        {
+            Mock.Get(singer).Setup(s => s.Sing()).Returns(lyrics);
+            Assert.That(song.GetLyrics(), Is.EqualTo(lyrics));
+        }
+    }
+
+    public class AutoMoqDataAttribute : AutoDataAttribute
+    {
+        public AutoMoqDataAttribute() : base(new Fixture().Customize(new AutoMoqCustomization()))
+        {
+
+        }
+    }
+}
---
+++ AutoMoqDummy/ISinger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutoMoqDummy
+{
+    public interface ISinger
+    {
+        string Sing();
+    }
+
+    public class Song {
+        ISinger singer;
+        public Song(ISinger singer)
+        {
+            this.singer = singer;
+        }
+
+        public string GetLyrics()
+        {
+            return this.singer.Sing();
+        }
+    }
+}
